da6a13138f0e2e5ffc9f5afdf7459ed1cb508336
Karl Vulpius <karlvulpius@gmail.com>
2022-06-03 14:19:37 +0200

    Fix project config

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.tsx":
+/*!***********************!*\
+  !*** ./src/index.tsx ***!
+  \***********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/karlrichard/projects/healing-platform/src/index.tsx: Support for the experimental syntax 'jsx' isn't currently enabled (6:3):\\n\\n\\u001b[0m \\u001b[90m 4 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 |\\u001b[39m \\u001b[33mReactDOM\\u001b[39m\\u001b[33m.\\u001b[39mrender(\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 6 |\\u001b[39m   \\u001b[33m<\\u001b[39m\\u001b[33mReact\\u001b[39m\\u001b[33m.\\u001b[39m\\u001b[33mStrictMode\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m   \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 7 |\\u001b[39m     \\u001b[33m<\\u001b[39m\\u001b[33mApp\\u001b[39m \\u001b[33m/\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 8 |\\u001b[39m   \\u001b[33m<\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33mReact\\u001b[39m\\u001b[33m.\\u001b[39m\\u001b[33mStrictMode\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[33m,\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 9 |\\u001b[39m   document\\u001b[33m.\\u001b[39mgetElementById(\\u001b[32m\\\"root\\\"\\u001b[39m)\\u001b[0m\\n\\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\\n    at Parser._raise (/Users/karlrichard/projects/healing-platform/node_modules/@babel/parser/src/parser/error.js:151:45)\\n    at Parser.raiseWithData (/Users/karlrichard/projects/healing-platform/node_modules/@babel/parser/src/parser/error.js:146:17)\\n    at Parser.expectOnePlugin (/Users/karlrichard/projects/healing-platform/node_modules/@babel/parser/src/parser/util.js:215:18)\\n    at Parser.parseExprAtom (/Users/karlrichard/projects/healing-platform/node_modules/@babel/parser/src/parser/expression.js:1228:16)\\n    at Parser.parseExprSubscripts (/Users/karlrichard/projects/healing-platform/node_modules/@babel/parser/src/parser/expression.js:680:23)\\n    at Parser.parseUpdate (/Users/karlrichard/projects/healing-platform/node_modules/@babel/parser/src/parser/expression.js:660:21)\\n    at Parser.parseMaybeUnary (/Users/karlrichard/projects/healing-platform/node_modules/@babel/parser/src/parser/expression.js:631:23)\\n    at Parser.parseMaybeUnaryOrPrivate (/Users/karlrichard/projects/healing-platform/node_modules/@babel/parser/src/parser/expression.js:386:14)\\n    at Parser.parseExprOps (/Users/karlrichard/projects/healing-platform/node_modules/@babel/parser/src/parser/expression.js:396:23)\\n    at Parser.parseMaybeConditional (/Users/karlrichard/projects/healing-platform/node_modules/@babel/parser/src/parser/expression.js:354:23)\");\n\n//# sourceURL=webpack://react-landing-form-checkout/./src/index.tsx?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.tsx"]();
+/******/ 	
+/******/ })()
+;
